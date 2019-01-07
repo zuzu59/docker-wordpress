1ee342c1ab85a8124f86a9fc13e047f506df3f13
Christian Zufferey <christian@zufferey.com>
2019-01-07 16:13:12 +0000

    Rendu plus efficace l'effacement TOTAL des installations de Docker

--- all_purge.sh
+++ all_purge.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #Petit script pour nettoyer tout le binz
 #ATTENTION, cela purge VRAIMENT tout le binz de Docker !
-#zf181003.1054
+#zf190107.1707
 
 ./list.sh
 read -p "Etes-vous certain de vouloir tout effacer ?"
+read -p "Mais cela va VRAIMENT VRAIMENT tout effacer!"
 
 docker container rm -f -v $(docker container ls -a -q)
 docker image rm -f $(docker image ls -a -q)
+docker volume rm $(docker volume ls -q)
 
-./list.sh
