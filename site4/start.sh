7c09eaba500b70c8682ab1067f88bcbf1bf46799
Christian Zufferey <christian@zufferey.com>
2019-01-24 10:22:32 +0100

    Recréé la structure des sites 2, 3 et 4 afin de pouvoir démarrer 4x sites wordpress

--- site4/start.sh
+++ site4/start.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
 #Petit script pour démarrer tout le binz
-#zf190107.1648
+#zf190115.1858
+
 #pour installer Docker et Docker compose sur une machine Ubuntu c'est ici:
 #https://github.com/zuzu59/docker_demo
 
-docker-compose up
-#docker-compose up -d
+#docker-compose up
+docker-compose up -d
+docker-compose logs -f
+
 
 echo -e "
 
+pour voir les logs en continu:
+docker-compose logs -f
 
-pour l'arrêter il faut faire:
+pour voir qu'est-ce qui tourne:
+docker-compose ps
 
+pour 'entrer' dans un 'service':
+docker-compose exec nom_service /bin/bash
+docker-compose exec wordpress /bin/bash
+docker-compose exec db /bin/bash
+
+pour arrêter:
 docker-compose stop
 
-
-pour le redémarrer il faut faire:
-
+pour redémarrer après un 'stop':
 docker-compose start
 
-
-pour enlever les container mais pas les datas il faut faire:
-
+pour enlever les container mais pas les datas:
 docker-compose down
+
+pour enlever les container ET aussi les datas:
+docker-compose down -v --remove-orphans
 
 
 "
