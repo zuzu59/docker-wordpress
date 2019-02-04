81a54732705a860a32c7bf3869fcefeb0a1570b9
Christian Zufferey <christian@zufferey.com>
2019-02-04 18:49:17 +0100

    Ajouté l'exportation du dossier wp-content afin de pouvoir sauvegarder plus facilement en remote ;-)

--- restore/start.sh
+++ restore/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#Petit script pour tester le restore
-#zf190114.1809
+#Petit script pour lancer la restauration facilement
+#zf190204.1846
 
-/root/restore/restore.sh toto.zip somewordpress!
+#/root/restore/restore.sh toto.zip somewordpress!
+/root/restore/restore.sh makerspace-lausanne-pw8000-1549298080-complete-2019-02-04-17-36-01.zip somewordpress!
