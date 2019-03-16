8a3ee0116b1648b1ae71dcce35b55c84f56b4605
Christian Zufferey <christian@zufferey.com>
2019-03-16 10:45:04 +0100

    wip

--- restore/start.sh
+++ restore/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Petit script pour lancer la restauration facilement
-#zf190204.1846
+#zf190204.1902
 
 #/root/restore/restore.sh toto.zip somewordpress!
-/root/restore/restore.sh makerspace-lausanne-pw8000-1549298080-complete-2019-02-04-17-36-01.zip somewordpress!
+/root/restore/restore.sh makerspace-lausanne-ch8000-1549302808-complete-2019-02-04-18-54-57.zip somewordpress!
