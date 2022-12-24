c0e25f5d4086ccd51ed3772ef6561d4ba9dc8408
Christian Zufferey <christian@zufferey.com>
2022-12-24 12:36:49 +0100

    new: ajouté la config apache2 dans le host pour pouvoir modifier apache2/conf-enabled/remoteip.conf afin de lui mettre RemoteIPInternalProxy 51.15.0.0/16 pour avoir la résolution des adresses ip externes dans les logs du container Wordpress

---
+++ stop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Petit script pour arrêter tout le binz
+#zf221224.1029
+
+#pour installer Docker et Docker compose sur une machine Ubuntu c'est ici:
+#https://github.com/zuzu59/docker_demo
+
+docker-compose stop
